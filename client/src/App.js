83b40d1fb0cdd3ca5f7fd49b9babf807206a25b3
“antonio150” <antoniorollande@gmail.com>
2023-04-05 11:56:33 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,121 @@
+import logo from './logo.svg';
+import './normal.css';
+import './App.css';
+import gptIcon from "./assets/chatgpt-icon.png"
+import {useState, useEffect } from 'react'
+
+function App() {
+
+ 
+// add state for input and chat log
+
+  const [input, setInput] = useState("");
+  const [models, setModels] = useState([]);
+  const [chatLog, setChatLog] = useState([{
+    user: "gpt",
+    message: "How can I help you today ?"
+  },{
+    user: "me",
+    message: "I want to use ChatGPT today"
+  }]);
+
+  // clear chats
+  function clearChat(){
+    setChatLog([]);
+  }
+
+ 
+
+
+  async function handleSubmit(e){
+    e.preventDefault();
+     let chatLogNew= [...chatLog, { user: "me", message: `${input}` }]
+     setInput ("");
+     setChatLog(chatLogNew)
+
+    // fetch response to the api combining the chat log array of messages and sending it as a message to localhost:3000 as a post
+
+    const messages = chatLogNew.map((message) => message.message).
+    join("\n")
+    const response = await fetch("http://localhost:3080", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: messages
+      })
+    });
+    const data = await response.json();
+    setChatLog([...chatLogNew, { user: "gpt", message: `${data.message}`}])
+    console.log(data.message);
+
+  }
+  return (
+    <div className="App">
+     
+        <aside className="sidemenu">
+          <div className='side-menu-button' onClick={clearChat}>
+            <span>+</span>
+            New Chat
+          </div>
+
+          <div className="models">
+           
+          </div>
+        </aside>
+
+        <section className="chatbox">
+          <div className="chat-log">
+
+            {chatLog.map((message, index) => (
+              <ChatMessage key={index} message={message} />
+            ))}
+            
+
+
+            
+
+          </div>
+          <div className='chat-input-holder'>
+            <form onSubmit={handleSubmit}>
+
+            
+            <input
+            rows="1"
+            
+            value={input}
+            onChange={(e) => setInput(e.target.value)  }
+            className='chat-input-textarea'
+            />
+           
+
+            </form>
+
+          </div>
+        </section>
+     
+     
+    </div>
+  );
+}
+
+const ChatMessage = ({ message }) => {
+  return(
+    <div className={`chat-message ${message.user === "gpt" && "chatgpt"}`}>
+      <div className='chat-message-center'>
+        <div className={`avatar ${message.user === "gpt" && "chatgpt"}`}>
+
+        </div>
+
+        <div className='message'>
+            {message.message}
+        </div>
+      </div>
+
+    </div>
+  )
+  }
+
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,63 @@
+// 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
+
+
+// import { Configuration, OpenAIApi } from "openai";
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express');
+
+
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+const configuration = new Configuration({
+    organization: "org-ul5eXL12sxPP8lPdMeUZXtBE", 
+    apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+// const response = await openai.listEngines(); 
+
+
+
+// create a simple express api that calls the function above
+
+
+
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+//  can you please add cors to express
+
+
+const port = 3080
+
+app.post('/', async (req, res) => {
+        const { message } = req.body;
+        console.log(message, "message")
+        
+        const response = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: `${message}`,
+                max_tokens: 100,
+                temperature: 0.5,
+        });
+
+        console.log()
+        res.json({
+                // data: response.data
+                message: response.data.choices[0].text,
+        })
+});
+
+app.get('/models', async (req, res) => {
+        const response = await openai.listEngines();
+        console.log(response.data.data)
+        res.json({
+                models: response.data.data
+        })
+});
+app.listen(port, () => {
+        console.log(`Exemple app listening at http://localhost:${port}`)
+});
+
